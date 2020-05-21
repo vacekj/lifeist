b876eddf1b61caaa262b17c780291db13c336700
Josef Vacek <vacekj@outlook.com>
2020-05-21 16:43:28 +0200

    feat: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./Components/Home";
 
 function App() {
-	return <div>Macek</div>;
+	return (
+		<div>
+			<Switch>
+				<Route path={"/"}>
+					<Home />
+				</Route>
+
+				<Route path={"/add"}>
+					<Home />
+				</Route>
+
+				<Route path={"/dashboard"}>
+					<Home />
+				</Route>
+
+				<Route path={"/goal/:id"}>
+					<Home />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
