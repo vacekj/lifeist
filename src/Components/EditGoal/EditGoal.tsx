df04f080d47dd26d93e75d33f291149eae44a992
Dominik Clemenete <dominik.clemente@gmail.com>
2020-05-23 16:13:49 +0100

    fix(EditGoal): dark mode UI

--- src/Components/EditGoal/EditGoal.tsx
+++ src/Components/EditGoal/EditGoal.tsx
@@ -7,13 +7,9 @@ const EditGoal = () => {
 	return (
 		<div className="flex flex-col">
 			{/* Header */}
-			<div className="flex items-center justify-between p-3 border-b border-gray-300">
+			<div className="flex items-center justify-between p-3">
 				<button>
-					<svg
-						fill="currentColor"
-						className={"text-gray-900 w-8 h-8"}
-						viewBox="0 0 20 20"
-					>
+					<svg fill="currentColor" className={"w-8 h-8"} viewBox="0 0 20 20">
 						<path
 							d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
 							clipRule="evenodd"
@@ -23,7 +19,7 @@ const EditGoal = () => {
 				</button>
 				<h1 className="text-2xl">Edit item</h1>
 				<button>
-					<svg className="text-gray-900 w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
+					<svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
 						<path
 							d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
 							clipRule="evenodd"
@@ -35,10 +31,10 @@ const EditGoal = () => {
 			{/* Form */}
 			<div>
 				<form className="flex flex-col justify-center w-full p-3">
-					<label className="text-gray-700">Name</label>
-					<input className="bg-gray-200 w-full mb-3 rounded h-10 p-2 " />
-					<label className="text-gray-700">Description</label>
-					<textarea className="resize-none bg-gray-200 w-full rounded h-40 p-2" />
+					<label>Name</label>
+					<input className="bg-background-lighter w-full mb-3 rounded h-10 p-2 " />
+					<label>Description</label>
+					<textarea className="resize-none bg-background-lighter w-full rounded h-40 p-2" />
 				</form>
 			</div>
 		</div>
