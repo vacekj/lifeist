80b139ddf48b5b077eec086e3d247baacbe8a4c8
Josef Vacek <vacekj@outlook.com>
2020-05-25 16:58:10 +0200

    add cloud functions to project

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,40 @@
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+import * as firebase from "firebase";
+import * as express from "express";
+
+import * as admin from "firebase-admin";
+
+const functions = require("firebase-functions");
+admin.initializeApp();
+
+export const getStats = functions.https.onRequest(
+	async (_: Request, response: express.Response) => {
+		try {
+			const snapshot = await admin
+				.firestore()
+				.collection("goals")
+				.get();
+			const data = snapshot.docs.map(g => g.data() as Goal);
+
+			/*Pick only anonymous data*/
+			return response.json(
+				data.map(goal => {
+					return { title: goal.title, completed: goal.completed };
+				})
+			);
+		} catch (e) {
+			return response.json(e);
+		}
+	}
+);
+
+type Goal = {
+	uid: string;
+	title: string;
+	description: string;
+	owner_uid: string;
+	archived: boolean;
+	completed: boolean;
+	completed_on: firebase.firestore.Timestamp;
+	created_at: firebase.firestore.Timestamp;
+	updated_at: firebase.firestore.Timestamp;
+};
